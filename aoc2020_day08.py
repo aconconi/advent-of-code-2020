16870e84c45b4e94625aa7b71887303f824a7c8c
Alex Conconi <alex.conconi@mi-002424-ap.local>
2020-12-08 12:01:18 +0100

    smarter solution for day 8 part 2

--- aoc2020_day08.py
+++ aoc2020_day08.py
@@ -3,25 +3,24 @@
     Day 08: Handheld Halting
 """
 
-from collections import Counter
 
 def read_puzzle_input(file_name):
-    '''Reads program from file and returns a program as list of (instruction, argument) tuples.'''
+    """Reads program from file and returns a program as list of (instruction, argument) tuples."""
     puzzle_input = open(file_name, "r").read().splitlines()
     return [(line.split()[0], int(line.split()[1])) for line in puzzle_input]
 
 
 def safe_run(program):
-    '''Runs the program, returns (True, acc) if it terminates, (False, acc) if a loop is detected.'''
-    reps = set()
+    """Runs the program, returns (True, acc) if it terminates, (False, acc) if a loop is detected."""
+    visited = set()
     pc = 0
     acc = 0
     while pc < len(program):
         instr, arg = program[pc]
-        if pc in reps:
+        if pc in visited:
             return (False, acc)
         else:
-            reps.add(pc)
+            visited.add(pc)
 
         if instr == "acc":
             acc += arg
@@ -39,7 +38,8 @@ def day08_part1(program):
     return acc
 
 
-def day08_part2(program):
+def day08_part2_bruteforce(program):
+    """Brute force approach to part 2"""
     # Try a first run withount changing anything because maybe the program is not corrupted at all.
     halts, acc = safe_run(program)
     if halts:
@@ -59,9 +59,43 @@ def day08_part2(program):
     raise Exception("Could not find a fix for this program.")
 
 
-# Part 1
+def day08_part2(program):
+    """Smarter solution for part 2"""
+    queue = [(0, 0)]  # (pc, acc)
+    visited = set()
+    while queue:
+        pc, acc = queue.pop(0)
+        while True:
+            if pc in visited:
+                break
+            visited.add(pc)
+
+            if pc == len(program):
+                return acc
+
+            instr, arg = program[pc]
+            if instr == "acc":
+                acc += arg
+                pc += 1
+            elif instr == "jmp":
+                if not (pc + 1, acc) in visited:
+                    queue.append((pc + 1, acc))
+                pc += arg
+            elif instr == "nop":
+                if not (pc + 1, acc) in visited:
+                    queue.append((pc + arg, acc))
+                pc += 1
+
+
+
+
+    # If we get here, something is wrong.
+    raise Exception("Smart: Could not find a fix for this program.")
+
+
+# # Part 1
 input_program = read_puzzle_input("data/day08.txt")
-print( "Immediately before any instruction is executed a second time, what value is in the accumulator?")
+print("Immediately before any instruction is executed a second time, what value is in the accumulator?")
 print(day08_part1(input_program))  # Correct answer is 2058.
 
 # Part 2
@@ -70,10 +104,12 @@ print(day08_part2(input_program))  # Correct answer is 1000.
 
 
 # Test cases
+test_program = read_puzzle_input("data/day08_test.txt")
+
+
 def test_day08_part1():
-    test_program = read_puzzle_input("data/day08_test.txt")
     assert day08_part1(test_program) == 5
 
+
 def test_day08_part2():
-    test_program = read_puzzle_input("data/day08_test.txt")
     assert day08_part2(test_program) == 8
