796df6e8a04df1e7c38404f56841f59572d0cb19
aconconi <4670015+aconconi@users.noreply.github.com>
2020-12-29 20:25:10 +0100

    Refactoring

--- aoc2020_day25.py
+++ aoc2020_day25.py
@@ -3,46 +3,43 @@
     Day 25: Combo Breaker
 """
 
-# INPUT = (door_public_key, card_public_key)
-TEST_INPUT = (17807724, 5764801)
-PUZZLE_INPUT = (6929599, 2448427)
+SUBJECT = 7
+DIVIDER = 20201227
+
+
+def read_puzzle_input(file_name):
+    with open(file_name, "r") as data_file:
+        return tuple(map(int, data_file.read().splitlines()))
 
 
 def find_loop_size(pub_key):
     value = 1
-    subject = 7
     loop_size = 0
-    while True:
-        if value == pub_key:
-            return loop_size
+    while value != pub_key:
         loop_size += 1
-        value *= subject
-        value %= 20201227
+        value = (value * SUBJECT) % DIVIDER
+    return loop_size
 
 
 def transform(subject, loop_size):
-    value = 1
-    for _ in range(loop_size):
-        value *= subject
-        value %= 20201227
-    return value
+    return pow(subject, loop_size, DIVIDER)
 
 
 def day25_part1(data):
     door_pub, card_pub = data
-    door_ls = find_loop_size(door_pub)
-    card_ls = find_loop_size(card_pub)
-    key1 = transform(door_pub, card_ls)
-    key2 = transform(card_pub, door_ls)
+    door_ls, card_ls = find_loop_size(door_pub), find_loop_size(card_pub)
+    key1, key2 = transform(door_pub, card_ls), transform(card_pub, door_ls)
     assert key1 == key2
     return key1
 
 
 if __name__ == "__main__":
     # Part 1 and 2 :-)
+    input_data = read_puzzle_input("data/day25.txt")
     print("What encryption key is the handshake trying to establish?")
-    print(day25_part1(PUZZLE_INPUT))
+    print(day25_part1(input_data))
 
 
 def test_day25_part1():
-    assert day25_part1(TEST_INPUT) == 14897079
+    test_case = (17807724, 5764801)
+    assert day25_part1(test_case) == 14897079
